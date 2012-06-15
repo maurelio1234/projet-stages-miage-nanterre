1d1632e011de0fcd59636cb19d8305eb4d7393b1
ines.kacouchia <ines.kacouchia@gmail.com>
2012-06-15 06:51:42 +0000

    Modification des classes Entreprise et Semestre

--- Projet_info/src/EtudiantStage/Periode.java
+++ Projet_info/src/EtudiantStage/Periode.java
@@ -1,5 +1,60 @@
 package EtudiantStage;
 
-public class Periode {
+import java.io.Serializable;
 
-}
+
+	public class Periode implements Serializable{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 3783245261993784176L;
+		private int numeroPeriode;
+		private Jours dateDebut, dateFin;
+		private Semestre monSemestre;
+		
+
+		public Periode() {
+			super();
+		}
+
+		public Periode(int numeroPeriode) {
+			this();
+			this.setNumeroPeriode(numeroPeriode);
+		}
+		
+
+
+		public int getNumeroPeriode() {
+			return numeroPeriode;
+		}
+
+		public void setNumeroPeriode(int numeroPeriode) {
+			this.numeroPeriode = numeroPeriode;
+		}
+
+		public Jours getDateDebut() {
+			return dateDebut;
+		}
+
+		public void setDateDebut(Jours dateDebut) {
+			this.dateDebut = dateDebut;
+		}
+
+		public Jours getDateFin() {
+			return dateFin;
+		}
+
+		public void setDateFin(Jours dateFin) {
+			this.dateFin = dateFin;
+		}
+
+		public Semestre getMonSemestre() {
+			return monSemestre;
+		}
+
+		public void setMonSemestre(Semestre monSemestre) {
+			this.monSemestre = monSemestre;
+		}
+		
+	}
+
--- Projet_info/src/EtudiantStage/Semestre.java
+++ Projet_info/src/EtudiantStage/Semestre.java
@@ -1,33 +1,66 @@
 package EtudiantStage;
 
-public class Semestre {
-	
-    private int NumeroSemestre;
-    private String Libelle;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Semestre implements Serializable{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -7879375760488149641L;
+	private int numeroSemestre;
+    private String libelle;
+    private List<UE> mesUE = new ArrayList<UE>();
+    private List<Periode> mesPeriodes = new ArrayList<Periode>();
 	
     public Semestre() {
+    	super();
 	}
 
 	public Semestre(int numeroSemestre, String libelle) {
-		NumeroSemestre = numeroSemestre;
-		Libelle = libelle;
+		this();
+		this.numeroSemestre = numeroSemestre;
+		this.libelle = libelle;
+	}
+	
+	public Semestre(int numeroSemestre){
+		this(numeroSemestre,"");
 	}
 
 	public int getNumeroSemestre() {
-		return NumeroSemestre;
+		return numeroSemestre;
 	}
 
 	public void setNumeroSemestre(int numeroSemestre) {
-		NumeroSemestre = numeroSemestre;
+		this.numeroSemestre = numeroSemestre;
 	}
 
 	public String getLibelle() {
-		return Libelle;
+		return libelle;
 	}
 
 	public void setLibelle(String libelle) {
-		Libelle = libelle;
+		this.libelle = libelle;
+	}
+
+	public List<UE> getMesUE() {
+		return mesUE;
+	}
+
+	public void setMesUE(List<UE> mesUE) {
+		this.mesUE = mesUE;
+	}
+
+	public List<Periode> getMesPeriodes() {
+		return mesPeriodes;
+	}
+
+	public void setMesPeriodes(List<Periode> mesPeriodes) {
+		this.mesPeriodes = mesPeriodes;
 	}
 }
+
 
 
