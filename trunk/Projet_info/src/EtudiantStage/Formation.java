a9da655bb0d5be60937a6dc17009e8bee86d6b4b
ines.kacouchia@gmail.com <ines.kacouchia@gmail.com@0c11541f-4425-afa0-34e3-8a9b68e5739a>
2012-06-15 07:07:52 +0000

    Modifications des classes Examen, Formation, Promotion, UE

--- trunk/Projet_info/src/EtudiantStage/Formation.java
+++ trunk/Projet_info/src/EtudiantStage/Formation.java
@@ -1,25 +1,41 @@
 package EtudiantStage;
 
-public class Formation {
-    private int numeroFormation;
-    private int numeroContrat;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Formation implements Serializable{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 7535003487189395864L;
+	private int numeroFormation;
+    private ContratQuadriennal monContrat;
+    private List<UE> mesUE = new ArrayList<UE>();
+    private List<Promotion> mesPromotions = new ArrayList<Promotion>();
     private String libelle;
     private String niveau;
     private String type;
     private String parcours;
 	
     public Formation() {
+    	super();
 	}
 
-	public Formation(int numeroFormation, int numeroContrat, String libelle, String niveau,String type, String parcours) {
+	public Formation(int numeroFormation, String libelle, String niveau,String type, String parcours) {
+		this();
 		this.numeroFormation = numeroFormation;
-		this.numeroContrat = numeroContrat;
 		this.libelle = libelle;
 		this.niveau = niveau;
 		this.type = type;
 		this.parcours = parcours;
 	}
-
+	
+	public Formation(int numeroFormation){
+		this(numeroFormation, "","","","");
+	}
+	
 	public int getNumeroFormation() {
 		return numeroFormation;
 	}
@@ -60,12 +76,28 @@ public class Formation {
 		this.parcours = parcours;
 	}
 
-	public int getNumeroContrat() {
-		return numeroContrat;
+
+	public ContratQuadriennal getMonContrat() {
+		return monContrat;
 	}
 
-	public void setNumeroContrat(int numeroContrat) {
-		this.numeroContrat = numeroContrat;
+	public void setMonContrat(ContratQuadriennal monContrat) {
+		this.monContrat = monContrat;
 	}
 
+	public List<UE> getMesUE() {
+		return mesUE;
+	}
+
+	public void setMesUE(List<UE> mesUE) {
+		this.mesUE = mesUE;
+	}
+
+	public List<Promotion> getMesPromotions() {
+		return mesPromotions;
+	}
+
+	public void setMesPromotions(List<Promotion> mesPromotions) {
+		this.mesPromotions = mesPromotions;
+	}
 }
--- trunk/Projet_info/src/EtudiantStage/UE.java
+++ trunk/Projet_info/src/EtudiantStage/UE.java
@@ -1,22 +1,60 @@
 package EtudiantStage;
 
-public class UE {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-	private int NumeroUE;
+
+public class UE implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3399982917151817441L;
+	private int numeroUE;
+	private Formation maFormation;
+	private List<EC> mesEC = new ArrayList<EC>();
+	private Semestre monSemestre;
 
 	public UE() {
+		super();
 	}
 
-	public UE(int numeroUE) {
-		NumeroUE = numeroUE;
+	public UE(int numeroUE, Formation maFormation) {
+		this();
+		this.numeroUE = numeroUE;
+		this.setMaFormation(maFormation);
 	}
 
 	public int getNumeroUE() {
-		return NumeroUE;
+		return numeroUE;
 	}
 
 	public void setNumeroUE(int numeroUE) {
-		NumeroUE = numeroUE;
+		this.numeroUE = numeroUE;
+	}
+
+	public Formation getMaFormation() {
+		return maFormation;
+	}
+
+	public void setMaFormation(Formation maFormation) {
+		this.maFormation = maFormation;
+	}
+
+	public List<EC> getMesEC() {
+		return mesEC;
+	}
+
+	public void setMesEC(List<EC> mesEC) {
+		this.mesEC = mesEC;
+	}
+
+	public Semestre getMonSemestre() {
+		return monSemestre;
+	}
+
+	public void setMonSemestre(Semestre monSemestre) {
+		this.monSemestre = monSemestre;
 	}
 }
 
