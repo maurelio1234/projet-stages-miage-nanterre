34af63abf3530a967f265d58135283f6cb94a88b
ines.kacouchia@gmail.com <ines.kacouchia@gmail.com@0c11541f-4425-afa0-34e3-8a9b68e5739a>
2012-06-15 09:27:13 +0000

    Création d'un Default Package

---
+++ trunk/Projet_info/src/TestOffreStage.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+
+
+public class TestOffreStage {
+
+private Entreprise uneEntreprise = new Entreprise();
+	
+	
+	public TestOffreStage(){
+		super();
+	}
+	
+	public void AfficherOffreStage(Entreprise e){
+		List<OffreDeStage> listeOffre = new List<OffreStage>();
+		e.getMesOffres();
+	}
+	
+}
