3422e585147848cbfcef2af3e640ff723f4a5b24
nafissadiop <nafissadiop@gmail.com>
2012-06-19 09:08:26 +0000

    Connection dans le constructeur

--- Projet_info/src/EtudiantDAO.java
+++ Projet_info/src/EtudiantDAO.java
@@ -1,3 +1,4 @@
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -16,19 +17,15 @@ public class EtudiantDAO extends DAO<Etudiant>{
 	private String UPDATE_ETUDIANT = "UPDATE etudiant SET ";
 	private String LIST_ETUDIANT = "SELECT * FROM candidat, etudiant WHERE candidat.no_candidat=etudiant.no_candidat";
 	
-	Connection newConnection() throws SQLException {
-	    final String url = "jdbc:mysql://localhost/miage";
-	    Connection conn = DriverManager.getConnection(url, "xacouder", "enhancer");
-	    return conn;
-	}
 	
-	public EtudiantDAO(Connection conn) {
-		super(conn);
+	public EtudiantDAO() throws SQLException {
+		
+		Connection cx = DriverManager.getConnection("jdbc:oracle:thin:@miage03.dmiage.u-paris10.fr:1521:MIAGE", "nadiop","finadiop");
+        Statement st=(Statement) cx.createStatement();
 		// TODO Auto-generated constructor stub
 	}
 
 	public Etudiant find(int numEtudiant) throws SQLException{
-		Connection conn = newConnection();
 		Etudiant etudiant = new Etudiant();
 		Candidat candidat = new Candidat();
 		ResultSet rs = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeQuery(FIND_ETUDIANT + numEtudiant);
@@ -43,7 +40,6 @@ public class EtudiantDAO extends DAO<Etudiant>{
 			candidat.setPassword(rs.getString("pasword"));
 			etudiant.setMonCandidat(candidat);
 		}
-		conn.close();
 		return etudiant;
 	}
 
