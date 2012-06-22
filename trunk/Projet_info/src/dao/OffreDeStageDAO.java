881be68defdcec104bae52dec492b10b678065a8
ines.kacouchia@gmail.com <ines.kacouchia@gmail.com@0c11541f-4425-afa0-34e3-8a9b68e5739a>
2012-06-22 15:01:04 +0000

    Mise à jour de OffreDeStageDAO en fonction du DAO de base (de l'équipe Integration)

--- trunk/Projet_info/src/dao/OffreDeStageDAO.java
+++ trunk/Projet_info/src/dao/OffreDeStageDAO.java
@@ -13,6 +13,7 @@ import java.util.GregorianCalendar;
 import beans.Entreprise;
 import beans.Jours;
 import beans.OffreDeStage;
+import dao.DAO;
 
 public class OffreDeStageDAO extends DAO<OffreDeStage>{
 	
@@ -25,7 +26,7 @@ public class OffreDeStageDAO extends DAO<OffreDeStage>{
 	private String FIND_ENTREPRISE = "SELECT * FROM ENTREPRISE WHERE NO_ENTREPRISE =";
 	
 	private Statement st;
-	
+	private SimpleDateFormat sf;
 	
 	public OffreDeStageDAO() throws SQLException {
         Connection cx=DriverManager.getConnection("jdbc:oracle:thin:@miage03.dmiage.u-paris10.fr:"
@@ -35,43 +36,76 @@ public class OffreDeStageDAO extends DAO<OffreDeStage>{
 		// TODO Auto-generated constructor stub
 	}
 	@Override
-	public void create(OffreDeStage ods) throws SQLException {
-		st.executeQuery(CREATE_OFFRESTAGE + ods.getNumeroOffreDeStage() + "," + ods.getDescriptionPoste() + "," + ods.getEtatOffre() + "," 
-				+ ods.getMonEntreprise().getNumeroEntreprise() + "," + ods.getDateFinStage() + "," + ods.getDateDebutStage());
+	public OffreDeStage create(OffreDeStage ods) {
+		ResultSet rs;
+		try {
+			rs = st.executeQuery(CREATE_OFFRESTAGE + ods.getNumeroOffreDeStage() + "," + ods.getDescriptionPoste() + "," + ods.getEtatOffre() + "," 
+					+ ods.getMonEntreprise().getNumeroEntreprise() + "," + ods.getDateFinStage() + "," + ods.getDateDebutStage());
+			while(rs.next()){
+				ods.setNumeroOffreDeStage(rs.getInt("NO_OFFRE")); 
+				ods.setDescriptionPoste(rs.getString("DESCRIPTION_OFFRE"));
+				ods.setEtatOffre(rs.getString("ETAT_OFFRE"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ods;
 		
 	}
 
 	@Override
-	public void delete(OffreDeStage ods) throws SQLException {
-		st.executeQuery(DELETE_OFFRESTAGE + ods.getNumeroOffreDeStage());
+	public void delete(OffreDeStage ods) {
+		try {
+			st.executeQuery(DELETE_OFFRESTAGE + ods.getNumeroOffreDeStage());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
-	public void update(OffreDeStage ods) throws SQLException {
-		st.executeQuery(UPDATE_OFFRESTAGE + ods.getNumeroOffreDeStage());
+	public OffreDeStage update(OffreDeStage ods) {
+		
+		ResultSet rs;
+		try {
+			rs = st.executeQuery(UPDATE_OFFRESTAGE + ods.getNumeroOffreDeStage());
+			while(rs.next()){
+				ods.setNumeroOffreDeStage(rs.getInt("NO_OFFRE")); 
+				ods.setDescriptionPoste(rs.getString("DESCRIPTION_OFFRE"));
+				ods.setEtatOffre(rs.getString("ETAT_OFFRE"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ods;
 		
 	}
 
 	@Override
-	public OffreDeStage find(int id) throws SQLException {
+	public OffreDeStage find(int id) {
 		OffreDeStage ods = new OffreDeStage();
-		Entreprise ent = new Entreprise();
-//		EntrepriseDAO etpDAO = new EntrepriseDAO();
+//		Entreprise ent = new Entreprise();
 		Jours jr = new Jours();
 		 
-		ResultSet rs = st.executeQuery(FIND_OFFRESTAGE + id);
+		ResultSet rs;
+		try {
+			rs = st.executeQuery(FIND_OFFRESTAGE + id);
+		
 //		ResultSet rs1 = st.executeQuery(NO_ENT_OFFRESTAGE + id);
 		
-		while(rs.next()){
-			System.out.println("Je suis dans la 1ere boucle");
-			ods.setNumeroOffreDeStage(rs.getInt("NO_OFFRE")); 
-			System.out.println("Je fais no offre: " + rs.getInt("NO_OFFRE"));
-            ods.setDescriptionPoste(rs.getString("DESCRIPTION_OFFRE"));
-            System.out.println("Je fais description offre: " + rs.getString("DESCRIPTION_OFFRE"));
-            ods.setEtatOffre(rs.getString("ETAT_OFFRE"));
-            System.out.println("Je fais etat offre: " + rs.getString("ETAT_OFFRE"));
-            
+		try {
+			while(rs.next()){
+				System.out.println("Je suis dans la 1ere boucle");
+				ods.setNumeroOffreDeStage(rs.getInt("NO_OFFRE")); 
+				System.out.println("Je fais no offre: " + rs.getInt("NO_OFFRE"));
+			    ods.setDescriptionPoste(rs.getString("DESCRIPTION_OFFRE"));
+			    System.out.println("Je fais description offre: " + rs.getString("DESCRIPTION_OFFRE"));
+			    ods.setEtatOffre(rs.getString("ETAT_OFFRE"));
+			    System.out.println("Je fais etat offre: " + rs.getString("ETAT_OFFRE"));
+			    
 //    		ResultSet rs2 = st.executeQuery(FIND_ENTREPRISE + rs.getInt("NO_ENTREPRISE"));
 //    		System.out.println("Je fais No entreprise: " + rs.getString("NO_NETREPRISE"));
 //    		while(rs2.next()){
@@ -84,25 +118,38 @@ public class OffreDeStageDAO extends DAO<OffreDeStage>{
 //                ent.setPassword(rs.getString("MDP_ENTREPRISE"));
 //    		}
 //    		ods.setMonEntreprise(ent);
-    		
+				
 //            Entreprise etp = etpDAO.find(rs.getInt("NO_ENTREPRISE"));
 //            etp.setNumeroEntreprise(rs.getInt("NO_ENTREPRISE"));
 //            ods.setMonEntreprise(etp);
-            
-            GregorianCalendar jourDebut = asCalendar(rs.getDate("DATE_DEBUT_STAGE"));
-			jr.setDateDuJour(jourDebut);
-			ods.setDateDebutStage(jr);
-			
-			GregorianCalendar jourFin = asCalendar(rs.getDate("DATE_FIN_STAGE"));
-			jr.setDateDuJour(jourFin);
-			ods.setDateFinStage(jr);       
+			    
+			    GregorianCalendar jourDebut = asCalendar(rs.getDate("DATE_DEBUT_STAGE"));
+				jr.setDateDuJour(jourDebut);
+				ods.setDateDebutStage(jr);
+				System.out.println("Je fais Date debut: " + jourDebut);
+				
+				GregorianCalendar jourFin = asCalendar(rs.getDate("DATE_FIN_STAGE"));
+				jr.setDateDuJour(jourFin);
+				ods.setDateFinStage(jr);  
+				
+				String jourFinS = stringToCalendar(jourFin);
+				System.out.println("Je fais Date fin: " + jourFinS);
+				
+				
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		System.out.println("Numero offre" + ods.getNumeroOffreDeStage());
-		System.out.println("Description offre" + ods.getDescriptionPoste());
-		System.out.println("Etat offre" + ods.getEtatOffre());
-		System.out.println("Date debut" + ods.getDateDebutStage());
-		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return ods;
 	}
 	
@@ -113,14 +160,17 @@ public class OffreDeStageDAO extends DAO<OffreDeStage>{
 	}
 	
 	
-	public static GregorianCalendar stringToCalendar(String sDate) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Date date = (Date) sdf.parse(sDate);
-        return asCalendar(date);
+	public static String stringToCalendar(GregorianCalendar sDate) throws Exception {
+		int year = sDate.get(GregorianCalendar.YEAR);
+		int month = sDate.get(GregorianCalendar.MONTH);
+		int day = sDate.get(GregorianCalendar.DAY_OF_MONTH);
+        
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String date = sdf.format(sDate);
+        return date;
 	} 
 	
 
-	@Override
 	public ArrayList<OffreDeStage> getList() throws SQLException {
 		ArrayList<OffreDeStage> listOffreStage = new ArrayList();
 		OffreDeStage ods = new OffreDeStage();
