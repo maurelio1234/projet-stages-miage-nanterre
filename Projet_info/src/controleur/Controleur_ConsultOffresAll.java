9dd72220a4c9a9c97cd99564f7fa88b82884f17c
nafissadiop <nafissadiop@gmail.com>
2012-06-28 09:04:16 +0000

    Suppression Controleur Etudiant

---
+++ Projet_info/src/controleur/Controleur_ConsultOffresAll.java
@@ -0,0 +1,42 @@
+package controleur;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controleur_ConsultOffresAll
+ */
+@WebServlet("/Controleur_ConsultOffresAll")
+public class Controleur_ConsultOffresAll extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controleur_ConsultOffresAll() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
