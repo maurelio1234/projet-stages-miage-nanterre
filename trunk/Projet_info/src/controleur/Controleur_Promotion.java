aff310ef64d5ae0ce9bdf16b1fcc6b6702805ca0
dav.vay@gmail.com <dav.vay@gmail.com@0c11541f-4425-afa0-34e3-8a9b68e5739a>
2012-06-28 09:17:20 +0000

    Modification Controleur_Promotion

--- trunk/Projet_info/src/controleur/Controleur_Promotion.java
+++ trunk/Projet_info/src/controleur/Controleur_Promotion.java
@@ -1,17 +1,31 @@
 package controleur;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.Formation;
+import beans.OffreDeStage;
+
+import dao.EntrepriseDAO;
+import dao.FormationDAO;
 
 /**
  * Servlet implementation class Controleur_Promotion
  */
 @WebServlet("/Controleur_Promotion")
+
 public class Controleur_Promotion extends HttpServlet {
+	private FormationDAO form;
+	private List<Formation> Forma = new ArrayList<Formation>();
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -27,6 +41,12 @@ public class Controleur_Promotion extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		form = new FormationDAO();
+		Forma=form.findAll();
+		HttpSession session = request.getSession(true);
+		session.setAttribute("Forma", Forma);	
+		RequestDispatcher disp=getServletContext().getRequestDispatcher("/gestion_promotion.jsp");
+		disp.forward(request, response);
 	}
 
 	/**
